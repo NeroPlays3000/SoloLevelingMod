722bfa1fc4be99aa8a09810ba9365d86ad621313
NeroPlays3000 <neroplays3000@gmail.com>
2023-06-13 19:38:49 +0530

    Initial commit

---
+++ src/main/java/net/neroplays3000/sololeveling/SoloLeveling.java
@@ -0,0 +1,39 @@
+package net.neroplays3000.sololeveling;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(SoloLeveling.MOD_ID)
+public class SoloLeveling
+{
+    public static final String MOD_ID = "sololeveling";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SoloLeveling() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
